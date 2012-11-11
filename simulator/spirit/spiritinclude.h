a1e91e01ba1392ed41bd0e5d80c07263e5de6309
Arun Saha <arunksaha@gmail.com>
2012-11-10 17:23:45 -0800

    removed duplicate directories and files

--- simulator/spirit/spiritinclude.h
+++
@@ -1,8 +0,0 @@
-//
-// Copyright 2009 Arun Saha
-//
-// #include <boost/spirit.hpp>
-// #include <boost/spirit/include/classic.hpp>
-
-// Assume the following directory under /usr/include
-#include <boost_1_52_0/boost/spirit/include/classic.hpp>
--- spirit/spiritinclude.h
+++
@@ -1,5 +0,0 @@
-//
-// Copyright 2009 Arun Saha
-//
-// #include <boost/spirit.hpp>
-#include <boost/spirit/include/classic.hpp>
